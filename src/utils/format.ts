40689dfc8f9017f50ecd297c4d535aa9e8e04e60
Felipe Mendes <zo.fmendes@gmail.com>
2022-01-25 16:19:23 -0300

    feat: add format utils

---
+++ src/utils/format.ts
@@ -0,0 +1,24 @@
+import { Types } from '@/declarations';
+import BigNumber from 'bignumber.js';
+
+export const toBigNumber = (num: Types.Number): BigNumber => {
+  return new BigNumber(Number(num));
+};
+
+export const exponential = (decimals: Types.Number): BigNumber => {
+  return new BigNumber(10).pow(toBigNumber(decimals));
+};
+
+export const applyDecimals = (
+  num: Types.Number,
+  decimals: Types.Decimals
+): BigNumber => {
+  return toBigNumber(num).dividedBy(exponential(decimals));
+};
+
+export const removeDecimals = (
+  num: Types.Number,
+  decimals: Types.Decimals
+): BigNumber => {
+  return toBigNumber(num).multipliedBy(exponential(decimals));
+};
