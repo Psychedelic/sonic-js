358cc3bc06c98fd63dc800c61243a852c0cd29f3
Felipe Mendes <zo.fmendes@gmail.com>
2022-01-25 12:10:50 -0300

    chore: add test environment

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: '<rootDir>/tests/custom-test-env.js',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@tests/(.*)': '<rootDir>/tests/$1',
+  },
+  modulePaths: ['<rootDir>/src'],
+};
---
+++ tests/index.test.ts
@@ -0,0 +1,5 @@
+describe('index', () => {
+  test('should be true', () => {
+    expect(true).toBe(true);
+  });
+});
