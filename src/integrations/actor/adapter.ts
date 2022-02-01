a004845725b84131c797b2d852386f532bd3c453
Felipe Mendes <zo.fmendes@gmail.com>
2022-02-01 09:25:33 -0300

    feat: actor adapter

---
+++ src/integrations/actor/adapter.ts
@@ -0,0 +1,89 @@
+import { Actor, ActorSubclass, Agent, HttpAgent } from '@dfinity/agent';
+import { IDL } from '@dfinity/candid';
+
+export class ActorAdapter implements ActorAdapter.Repository {
+  constructor(
+    private provider?: ActorAdapter.Provider,
+    private options: ActorAdapter.Options = {
+      host: 'https://boundary.ic0.app/',
+      whitelist: ['3xwpq-ziaaa-aaaah-qcn4a-cai'],
+    }
+  ) {}
+
+  async createActor<T>(
+    canisterId: string,
+    interfaceFactory: IDL.InterfaceFactory
+  ): Promise<ActorSubclass<T>> {
+    if (ActorAdapter.Actors[canisterId]) {
+      return ActorAdapter.Actors[canisterId];
+    }
+
+    let actor;
+
+    if (!this.provider) {
+      const agent = new HttpAgent({ host: this.options.host });
+
+      actor = Actor.createActor<T>(interfaceFactory, {
+        agent,
+        canisterId,
+      });
+    } else {
+      await this.createAgent();
+      actor = await this.provider.createActor<T>({
+        canisterId,
+        interfaceFactory,
+      });
+    }
+
+    ActorAdapter.Actors[canisterId] = actor;
+    return actor;
+  }
+
+  private async createAgent(): Promise<void> {
+    if (this.provider && !this.provider?.agent) {
+      await this.provider.createAgent({
+        whitelist: this.options?.whitelist,
+        host: this.options.host,
+      });
+    }
+  }
+}
+
+export namespace ActorAdapter {
+  export const Actors: Record<string, ActorSubclass<any>> = {};
+
+  export type Provider = {
+    agent: Agent | null;
+    createActor<T>(params: CreateActor<T>): Promise<ActorSubclass<T>>;
+    createAgent(params: CreateAgentParams): Promise<Agent>;
+  };
+
+  export type Options = {
+    whitelist?: string[];
+    host?: string;
+  };
+
+  export interface Repository {
+    createActor: <T>(
+      canisterId: string,
+      interfaceFactory: IDL.InterfaceFactory
+    ) => Promise<T>;
+  }
+
+  export interface CreateActor<T> {
+    agent?: HttpAgent;
+    actor?: ActorSubclass<ActorSubclass<T>>;
+    canisterId: string;
+    interfaceFactory: IDL.InterfaceFactory;
+  }
+
+  export interface CreateAgentParams {
+    whitelist?: string[];
+    host?: string;
+  }
+
+  export type ActorProps = {
+    canisterId?: string;
+    interfaceFactory: IDL.InterfaceFactory;
+  };
+}
