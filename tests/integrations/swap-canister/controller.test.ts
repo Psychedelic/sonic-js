e866090552ff170b0e4f5eb77fca7716778465d6
Felipe Mendes <zo.fmendes@gmail.com>
2022-02-02 12:10:42 -0300

    test: test getPairList

--- tests/integrations/swap-canister/controller.test.ts
+++ tests/integrations/swap-canister/controller.test.ts
@@ -1,10 +1,12 @@
 import { SwapCanisterController } from 'integrations';
 import { mockSwapActor } from '../../mocks/actor';
+import { mockAllPairsResponse } from '../../mocks/pair';
 import { mockSupportedTokenListResponse } from '../../mocks/token';
 
 describe('SwapCanisterController', () => {
   let sut: SwapCanisterController;
   const supportedTokenListResponseMock = mockSupportedTokenListResponse();
+  const allPairsResponseMock = mockAllPairsResponse();
 
   beforeEach(() => {
     sut = new SwapCanisterController(mockSwapActor());
@@ -25,6 +27,39 @@ describe('SwapCanisterController', () => {
       expect(sut.tokenList).toBeDefined();
       supportedTokenListResponseMock.forEach((mock) => {
         expect(sut.tokenList?.[mock.id]).toEqual(mock);
+      });
+    });
+  });
+
+  describe('.getPairsList', () => {
+    test('should return a parsed list of pairs', async () => {
+      const response = await sut.getPairList();
+
+      allPairsResponseMock.forEach((mock) => {
+        expect(response[mock.token0][mock.token1]).toEqual(mock);
+        expect(response[mock.token1][mock.token0]).toEqual({
+          ...mock,
+          token0: mock.token1,
+          token1: mock.token0,
+          reserve0: mock.reserve1,
+          reserve1: mock.reserve0,
+        });
+      });
+    });
+
+    test('should store the response inside the class', async () => {
+      await sut.getPairList();
+
+      expect(sut.pairList).toBeDefined();
+      allPairsResponseMock.forEach((mock) => {
+        expect(sut.pairList?.[mock.token0][mock.token1]).toEqual(mock);
+        expect(sut.pairList?.[mock.token1][mock.token0]).toEqual({
+          ...mock,
+          token0: mock.token1,
+          token1: mock.token0,
+          reserve0: mock.reserve1,
+          reserve1: mock.reserve0,
+        });
       });
     });
   });
--- tests/mocks/actor.ts
+++ tests/mocks/actor.ts
@@ -1,7 +1,9 @@
 import { SwapActor } from '@/integrations';
+import { mockAllPairsResponse } from './pair';
 import { mockSupportedTokenListResponse } from './token';
 
 export const mockSwapActor = (): SwapActor =>
   ({
     getSupportedTokenList: async () => mockSupportedTokenListResponse(),
+    getAllPairs: async () => mockAllPairsResponse(),
   } as SwapActor);
